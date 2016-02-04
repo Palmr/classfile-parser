6d4e237cb5a3b00dedf9950e67261733538d21eb
Nick Palmer <nick@palmr.co.uk>
2016-02-04 21:42:28 +0000

    Stubbed field implementation and general refactoring

--- src/parser.rs
+++ src/parser.rs
@@ -3,7 +3,29 @@ use nom::{  IResult,
             be_i32,be_f32,
             ErrorKind,Err};
 
-pub enum ConstEntry {
+
+pub struct ClassFile {
+    pub minor_version: u16,
+    pub major_version: u16,
+    pub const_pool_size: u16,
+    pub const_pool: Vec<Constant>,
+    pub access_flags: u16,
+    pub this_class: u16,
+    pub super_class: u16,
+    pub interfaces_count: u16,
+    pub interfaces: Vec<u16>,
+    pub fields_count: u16,
+    pub fields: Vec<FieldInfo>,
+//     pub methods_count: u16,
+//     pub methods[methods_count]: u16,
+//     pub attributes_count: u16,
+//     pub attributes[attributes_count]: u16,
+}
+
+named!(magic_ident, tag!(&[0xCA, 0xFE, 0xBA, 0xBE]));
+
+// TODO - NP - Move this lot to a constants mod
+pub enum Constant {
     Utf8(Utf8Constant),
     Integer(IntegerConstant),
     Float(FloatConstant),
@@ -20,37 +42,18 @@ pub enum ConstEntry {
 //    InvokeDynamic(InvokeDynamicConstant),
 }
 
-impl ConstEntry {
+impl Constant {
     pub fn to_string(&self) -> String {
         match *self {
-            /*
-             * For all of the below matches, a reference to
-             * the underlying struct must be used; this is because we have
-             * borrowed self, and thus cannot take
-             * ownership of anything owned by self.
-             */
-            ConstEntry::Utf8(ref s) => format!(
-                "Utf8Constant[utf8_string=\"{}\"]", s.utf8_string),
-            ConstEntry::Integer(ref s) => format!(
-                "IntegerConstant[value=\"{}\"]", s.value),
-            ConstEntry::Float(ref s) => format!(
-                "FloatConstant[value=\"{}\"]", s.value),
-            ConstEntry::Class(ref s) => format!(
-                "ClassConstant[name_index={}]", s.name_index),
-            ConstEntry::String(ref s) => format!(
-                "StringConstant[string_index={}]", s.string_index),
-            ConstEntry::FieldRef(ref s) => format!(
-                "FieldRefConstant[class_index={}, name_and_type_index={}]",
-                    s.class_index, s.name_and_type_index),
-            ConstEntry::MethodRef(ref s) => format!(
-                "MethodRefConstant[class_index={}, name_and_type_index={}]",
-                    s.class_index, s.name_and_type_index),
-            ConstEntry::InterfaceMethodRef(ref s) => format!(
-                "InterfaceMethodRefConstant[class_index={}, name_and_type_index={}]",
-                    s.class_index, s.name_and_type_index),
-            ConstEntry::NameAndType(ref s) => format!(
-                "NameAndTypeConstant[name_index={}, descriptor_index={}]",
-                    s.name_index, s.descriptor_index),
+            Constant::Utf8(ref s) => format!("Utf8Constant[utf8_string=\"{}\"]", s.utf8_string),
+            Constant::Integer(ref s) => format!("IntegerConstant[value=\"{}\"]", s.value),
+            Constant::Float(ref s) => format!("FloatConstant[value=\"{}\"]", s.value),
+            Constant::Class(ref s) => format!("ClassConstant[name_index={}]", s.name_index),
+            Constant::String(ref s) => format!("StringConstant[string_index={}]", s.string_index),
+            Constant::FieldRef(ref s) => format!("FieldRefConstant[class_index={}, name_and_type_index={}]", s.class_index, s.name_and_type_index),
+            Constant::MethodRef(ref s) => format!("MethodRefConstant[class_index={}, name_and_type_index={}]", s.class_index, s.name_and_type_index),
+            Constant::InterfaceMethodRef(ref s) => format!("InterfaceMethodRefConstant[class_index={}, name_and_type_index={}]", s.class_index, s.name_and_type_index),
+            Constant::NameAndType(ref s) => format!("NameAndTypeConstant[name_index={}, descriptor_index={}]", s.name_index, s.descriptor_index),
         }
     }
 }
@@ -94,32 +97,12 @@ pub struct NameAndTypeConstant {
     pub descriptor_index: u16,
 }
 
-pub struct ClassFile {
-    pub minor_version: u16,
-    pub major_version: u16,
-    pub const_pool_size: u16,
-    pub const_pool: Vec<ConstEntry>,
-    pub access_flags: u16,
-    pub this_class: u16,
-    pub super_class: u16,
-    pub interfaces_count: u16,
-//     pub interfaces[interfaces_count]: u16,
-//     pub fields_count: u16,
-//     pub fields[fields_count]: u16,
-//     pub methods_count: u16,
-//     pub methods[methods_count]: u16,
-//     pub attributes_count: u16,
-//     pub attributes[attributes_count]: u16,
-}
-
-named!(magic_ident, tag!(&[0xCA, 0xFE, 0xBA, 0xBE]));
-
-named!(const_utf8<&[u8], ConstEntry>, chain!(
+named!(const_utf8<&[u8], Constant>, chain!(
     // tag!([0x01]) ~
     length: be_u16 ~
     utf8_str: take_str!(length),
     || {
-        ConstEntry::Utf8(
+        Constant::Utf8(
             Utf8Constant {
                 utf8_string: utf8_str.to_owned(),
             }
@@ -127,11 +110,11 @@ named!(const_utf8<&[u8], ConstEntry>, chain!(
     }
 ));
 
-named!(const_integer<&[u8], ConstEntry>, chain!(
+named!(const_integer<&[u8], Constant>, chain!(
     // tag!([0x03]) ~
     value: be_i32,
     || {
-        ConstEntry::Integer(
+        Constant::Integer(
             IntegerConstant {
                 value: value,
             }
@@ -139,11 +122,11 @@ named!(const_integer<&[u8], ConstEntry>, chain!(
     }
 ));
 
-named!(const_float<&[u8], ConstEntry>, chain!(
+named!(const_float<&[u8], Constant>, chain!(
     // tag!([0x04]) ~
     value: be_f32,
     || {
-        ConstEntry::Float(
+        Constant::Float(
             FloatConstant {
                 value: value,
             }
@@ -151,11 +134,11 @@ named!(const_float<&[u8], ConstEntry>, chain!(
     }
 ));
 
-named!(const_class<&[u8], ConstEntry>, chain!(
+named!(const_class<&[u8], Constant>, chain!(
     // tag: tag!([0x07]) ~
     name_index: be_u16,
     || {
-        ConstEntry::Class(
+        Constant::Class(
             ClassConstant {
                 name_index: name_index,
             }
@@ -163,11 +146,11 @@ named!(const_class<&[u8], ConstEntry>, chain!(
     }
 ));
 
-named!(const_string<&[u8], ConstEntry>, chain!(
+named!(const_string<&[u8], Constant>, chain!(
     // tag: tag!([0x08]) ~
     string_index: be_u16,
     || {
-        ConstEntry::String(
+        Constant::String(
             StringConstant {
                 string_index: string_index,
             }
@@ -175,12 +158,12 @@ named!(const_string<&[u8], ConstEntry>, chain!(
     }
 ));
 
-named!(const_field_ref<&[u8], ConstEntry>, chain!(
+named!(const_field_ref<&[u8], Constant>, chain!(
     // tag: tag!([0x09]) ~
     class_index: be_u16 ~
     name_and_type_index: be_u16,
     || {
-        ConstEntry::FieldRef(
+        Constant::FieldRef(
             FieldRefConstant {
                 class_index: class_index,
                 name_and_type_index: name_and_type_index,
@@ -189,12 +172,12 @@ named!(const_field_ref<&[u8], ConstEntry>, chain!(
     }
 ));
 
-named!(const_method_ref<&[u8], ConstEntry>, chain!(
+named!(const_method_ref<&[u8], Constant>, chain!(
     // tag: tag!([0x0A]) ~
     class_index: be_u16 ~
     name_and_type_index: be_u16,
     || {
-        ConstEntry::MethodRef(
+        Constant::MethodRef(
             MethodRefConstant {
                 class_index: class_index,
                 name_and_type_index: name_and_type_index,
@@ -203,12 +186,12 @@ named!(const_method_ref<&[u8], ConstEntry>, chain!(
     }
 ));
 
-named!(const_interface_method_ref<&[u8], ConstEntry>, chain!(
+named!(const_interface_method_ref<&[u8], Constant>, chain!(
     // tag: tag!([0x0B]) ~
     class_index: be_u16 ~
     name_and_type_index: be_u16,
     || {
-        ConstEntry::InterfaceMethodRef(
+        Constant::InterfaceMethodRef(
             InterfaceMethodRefConstant {
                 class_index: class_index,
                 name_and_type_index: name_and_type_index,
@@ -217,12 +200,12 @@ named!(const_interface_method_ref<&[u8], ConstEntry>, chain!(
     }
 ));
 
-named!(const_name_and_type<&[u8], ConstEntry>, chain!(
+named!(const_name_and_type<&[u8], Constant>, chain!(
     // tag: tag!([0x0C]) ~
     name_index: be_u16 ~
     descriptor_index: be_u16,
     || {
-        ConstEntry::NameAndType(
+        Constant::NameAndType(
             NameAndTypeConstant {
                 name_index: name_index,
                 descriptor_index: descriptor_index,
@@ -231,7 +214,7 @@ named!(const_name_and_type<&[u8], ConstEntry>, chain!(
     }
 ));
 
-pub fn parse_const(input: &[u8]) -> IResult<&[u8], ConstEntry> {
+pub fn parse_const(input: &[u8]) -> IResult<&[u8], Constant> {
     chain!(input,
         const_type: be_u8 ~
         const_block: apply!(const_block, const_type),
@@ -240,7 +223,7 @@ pub fn parse_const(input: &[u8]) -> IResult<&[u8], ConstEntry> {
         }
     )
 }
-pub fn const_block(input: &[u8], const_type: u8) -> IResult<&[u8], ConstEntry> {
+pub fn const_block(input: &[u8], const_type: u8) -> IResult<&[u8], Constant> {
     match const_type {
         1 => const_utf8(input),
         3 => const_integer(input),
@@ -272,6 +255,41 @@ pub enum AccessFlags {
     Enum,       // 	0x4000 	Declared as an enum type
 }
 
+pub struct FieldInfo {
+    access_flags: u16,
+    name_index: u16,
+    descriptor_index: u16,
+    attributes_count: u16,
+    // attributes: Vec<attribute_info>,
+}
+pub fn parse_field(input: &[u8]) -> IResult<&[u8], FieldInfo> {
+    chain!(input,
+        access_flags: be_u16 ~
+        name_index: be_u16 ~
+        descriptor_index: be_u16 ~
+        attributes_count: be_u16,
+        || {
+            if attributes_count > 0 {
+                panic!("Attr count > 0  on field but attrs not implemented yet!");
+                /*
+The attributes defined by this specification as appearing in the attributes table of a field_info structure are ConstantValue (§4.7.2), Synthetic (§4.7.8), Signature (§4.7.9), Deprecated (§4.7.15), RuntimeVisibleAnnotations (§4.7.16) and RuntimeInvisibleAnnotations (§4.7.17).
+
+A Java Virtual Machine implementation must recognize and correctly read ConstantValue (§4.7.2) attributes found in the attributes table of a field_info structure. If a Java Virtual Machine implementation recognizes class files whose version number is 49.0 or above, it must recognize and correctly read Signature (§4.7.9), RuntimeVisibleAnnotations (§4.7.16) and RuntimeInvisibleAnnotations (§4.7.17) attributes found in the attributes table of a field_info structure of a class file whose version number is 49.0 or above.
+
+A Java Virtual Machine implementation is required to silently ignore any or all attributes that it does not recognize in the attributes table of a field_info structure. Attributes not defined in this specification are not allowed to affect the semantics of the class file, but only to provide additional descriptive information (§4.7.1).
+                */
+            }
+            FieldInfo {
+                access_flags: access_flags,
+                name_index: name_index,
+                descriptor_index: descriptor_index,
+                attributes_count: attributes_count,
+            }
+        }
+    )
+}
+
+
 pub fn parse_classfile(input: &[u8]) -> IResult<&[u8], ClassFile> {
   chain!(input,
     magic_ident ~
@@ -282,7 +300,10 @@ pub fn parse_classfile(input: &[u8]) -> IResult<&[u8], ClassFile> {
     access_flags: be_u16 ~
     this_class: be_u16 ~
     super_class: be_u16 ~
-    interfaces_count: be_u16,
+    interfaces_count: be_u16 ~
+    interfaces: count!(be_u16, interfaces_count as usize) ~
+    fields_count: be_u16 ~
+    fields: count!(parse_field, fields_count as usize),
     || {
         ClassFile {
             minor_version: minor_version,
@@ -293,6 +314,9 @@ pub fn parse_classfile(input: &[u8]) -> IResult<&[u8], ClassFile> {
             this_class: this_class,
             super_class: super_class,
             interfaces_count: interfaces_count,
+            interfaces: interfaces,
+            fields_count: fields_count,
+            fields: fields,
         }
     }
   )
@@ -300,16 +324,28 @@ pub fn parse_classfile(input: &[u8]) -> IResult<&[u8], ClassFile> {
 
 #[test]
 fn test_valid_class() {
-    let valid_class = include_bytes!("../assets/SimpleMath.class");
+    let valid_class = include_bytes!("../assets/BasicClass.class");
     let res = parse_classfile(valid_class);
     match res {
         IResult::Done(_, c) => {
-            println!("Valid class file, version {},{} const_pool({}), this=const[{}], super=const[{}], interfaces({})", c.major_version, c.minor_version, c.const_pool_size, c.this_class, c.super_class, c.interfaces_count);
+            println!("Valid class file, version {},{} const_pool({}), this=const[{}], super=const[{}], interfaces({}), fields({})", c.major_version, c.minor_version, c.const_pool_size, c.this_class, c.super_class, c.interfaces_count, c.fields_count);
             println!("Constant pool:");
             let mut const_index = 1;
             for f in &c.const_pool {
                 println!("\t[{}] = {}", const_index, f.to_string());
                 const_index += 1;
+            }
+            println!("Interfaces:");
+            let mut interface_index = 0;
+            for i in &c.interfaces {
+                println!("\t[{}] = const[{}] = {}", interface_index, i, c.const_pool[(i-1) as usize].to_string());
+                interface_index += 1;
+            }
+            println!("Fields:");
+            let mut field_index = 0;
+            for f in &c.fields {
+                println!("\t[{}] Name(const[{}] = {})", field_index, f.name_index, c.const_pool[(f.name_index - 1) as usize].to_string());
+                field_index += 1;
             }
         },
         _ => panic!("Not a class file"),
@@ -338,7 +374,7 @@ fn test_constant_utf8() {
     match res {
         IResult::Done(_, c) =>
         match c {
-            ConstEntry::Utf8(ref s) =>
+            Constant::Utf8(ref s) =>
                  println!("Valid UTF8 const: {}", s.utf8_string),
             _ => panic!("It's a const, but of what type?")
         },
