2300f55211f7b5b62246df0748551f1fe02ed282
Nick Palmer <nick@palmr.co.uk>
2016-02-04 21:41:59 +0000

    Updated java assets

---
+++ assets/BasicInterface.java
@@ -0,0 +1,5 @@
+package uk.co.palmr.karl.examples;
+
+public interface BasicInterface {
+  String getOutputMessage();
+}
---
+++ assets/InterfaceImplementor.java
@@ -0,0 +1,14 @@
+package uk.co.palmr.karl.examples;
+
+public class InterfaceImplementor implements BasicInterface {
+
+  public static void main(String[] args) {
+    InterfaceImplementor object = new InterfaceImplementor();
+    System.out.println(object.getOutputMessage());
+  }
+
+  @Override
+  public String getOutputMessage() {
+    return "Output message from interface impl";
+  }
+}
