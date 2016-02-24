b90ee875b5926bf9213583c966c8b1737aca1319
Nick Palmer <nick@palmr.co.uk>
2016-02-24 22:28:54 +0000

    Added a parse_class method to work with files and handle nom errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 #[macro_use]
 extern crate nom;
+use nom::IResult;
 
 pub mod constant_info;
 pub mod attribute_info;
@@ -11,3 +17,26 @@ pub mod parser;
 
 pub use parser::class_parser;
 pub use types::ClassFile;
+
+pub fn parse_class(class_name: &str) -> Result<ClassFile, String> {
+    let class_file_name = &format!("{}.class", class_name);
+    let path = Path::new(class_file_name);
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => return Err(format!("Unable to open {}: {}", display, Error::description(&why))),
+        Ok(file) => file,
+    };
+
+    let mut class_bytes = Vec::new();
+    match file.read_to_end(&mut class_bytes) {
+        Err(why) => return Err(format!("Unable to read {}: {}", display, Error::description(&why))),
+        Ok(_) => println!("{} contains {} bytes.", display, class_bytes.len()),
+    };
+
+    let parsed_class = class_parser(&class_bytes);
+    match parsed_class {
+        IResult::Done(_, c) => Ok(c),
+        _ => Err("Failed to parse class?".to_string()),
+    }
+}
